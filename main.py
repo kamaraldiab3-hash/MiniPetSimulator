50b3238084f5476ee25897089a58a57ee1cb6e29
leen527 <leenroumani@gmail.com>
2025-12-27 06:52:36 -0800

    add feed_pet feature

---
+++ main.py
@@ -0,0 +1,8 @@
+from pet import Pet
+
+my_pet = Pet("Whiskers", 50) 
+
+print(f"Pet Name: {my_pet.name}")
+
+print(f"Energy Level: {my_pet.energy_level}")
+my_pet.feed_pet()
---
+++ pet.py
@@ -0,0 +1,8 @@
+class Pet:
+    def init(self, name, energy_level):
+        self.name = name
+        self.energy_level = energy_level
+        # أضف هذا التابع داخل كلاس Pet
+def feed_pet(self):
+    self.energy_level += 20
+    print(f"{self.name} has been fed! Energy: {self.energy_level}")
